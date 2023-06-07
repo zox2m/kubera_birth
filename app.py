c197551d632e5b430957710e8d964bbedb744ff2
zox2m <zox2mm@gmail.com>
2023-06-07 11:39:58 +0900

    last

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-from flask import Flask,render_template,request
+from flask import Flask,render_template,request,url_for
 import birth_attribute
 app = Flask(__name__)
 
-@app.route('/',methods=['GET','POST']) # 첫 화면
+@app.route('/') # 첫 화면
 def index():
     month=None
     day=None
@@ -13,8 +13,11 @@ def index():
         day = request.form['day']
         time = request.form['time']
 
-    return render_template('result.html',month=month, day=day,time=time)
+    return render_template('index.html',month=month, day=day,time=time)
 
+@app.route('/result') # 첫 화면
+def result():
+    return
 
 
 if __name__ == '__main__':
